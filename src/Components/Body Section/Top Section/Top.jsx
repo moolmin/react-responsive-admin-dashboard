d2a24dae7fcb616bbe3b4b7698b1026c5efcbae3
moolmin <zzmzzm0065@gmail.com>
2024-07-03 10:52:35 +0900

    feature left and right card in top section

--- src/Components/Body Section/Top Section/Top.jsx
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -5,9 +5,12 @@ import './top.css'
 import { BiSearchAlt } from "react-icons/bi";
 import { TbMessageCircle } from "react-icons/tb";
 import { IoNotificationsOutline } from "react-icons/io5";
+import {BsArrowRight, BsQuestionCircle} from "react-icons/bs";
 
 // Imported Image ======>
 import img from "../../../Assets/user(1).png"
+import img2 from "../../../Assets/user(2).png"
+import video from "../../../Assets/video.mp4"
 
 const Top = () => {
     return (
@@ -35,7 +38,56 @@ const Top = () => {
             <div className="cardSection flex">
                 <div className="rightCard flex">
                     <h1>Create and sell extraordinary products</h1>
+                    <p>The world's fast growing industry today are natural made products!</p>
+
+                    <div className="buttons flex">
+                        <div className="btn">Explore More</div>
+                        <div className="btn transparent">Top Sellers</div>
+                    </div>
+
+                    <div className="videoDiv">
+                        <video src={video} autoPlay loop muted></video>
+                    </div>
                 </div>
+
+                <div className="leftCard felx">
+                    <div className="main flex">
+
+                        <div className="textDiv">
+                            <h1>My Stat</h1>
+
+                            <div className="flex">
+                                <span>
+                                    Today <br/> <small>4 Orders</small>
+                                </span>
+                                <span>
+                                    This Month <br/> <small>127 Orders</small>
+                                </span>
+                            </div>
+
+                            <span className="flex link">
+                                Go to my orders <BsArrowRight className="icon"/>
+                            </span>
+                        </div>
+
+                        <div className="imgDiv">
+                            <img src={img2} alt="User2"/>
+                        </div>
+
+                        {/*<div className="sideBarCard">*/}
+                        {/*    <BsQuestionCircle className="icon"/>*/}
+                        {/*    <div className="cardContent">*/}
+                        {/*        <div className="circle1"></div>*/}
+                        {/*        <div className="circle1"></div>*/}
+                        {/*        <h3>Help Center</h3>*/}
+                        {/*        <p>Having troulble in Moolhub, please contact us from for more questions.</p>*/}
+                        {/*        <button className="btn">Go to help center</button>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
+
+                    </div>
+                </div>
+
             </div>
         </div>
     )
